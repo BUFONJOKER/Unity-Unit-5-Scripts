4cad167f13904634574a75cf1411b18f42d74d82
BUFONJOKER <abdulrehman9745@outlook.com>
2024-01-12 16:43:20 +0500

    till lesson 5.2

--- Target.cs
+++ Target.cs
@@ -10,34 +10,46 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPosition = -3;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         targetRigidBody = GetComponent<Rigidbody>();
         targetRigidBody.AddForce(RandomForce(), ForceMode.Impulse);
-        targetRigidBody.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(), ForceMode.Impulse);
+        targetRigidBody.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPosition();
     }
 
-    Vector3 RandomForce(){
+    Vector3 RandomForce()
+    {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
 
-    float RandomTorque(){
+    float RandomTorque()
+    {
         return Random.Range(-maxTorque, maxTorque);
     }
 
-    Vector3 RandomSpawnPosition(){
-        return new Vector3(Random.Range(-xRange,xRange),ySpawnPosition);
+    Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPosition);
     }
 
-    private void OnMouseDown(){
+    private void OnMouseDown()
+    {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+
     }
 
-    private void OnTriggerEnter(Collider other){
+    private void OnTriggerEnter(Collider other)
+    {
         Destroy(gameObject);
     }
     // Update is called once per frame
